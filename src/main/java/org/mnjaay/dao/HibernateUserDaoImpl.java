a9f538b695ea4401404a636d2953fd33150e8f72
Mamadou Ndiaye <mamadouamandiaye@gmail.com>
2025-05-30 11:47:19 +0000

    Added HibernateUserDaoImpl HibernateConnection and the hibernate configuration file hibernate.cfg.xml

---
+++ src/main/java/org/mnjaay/dao/HibernateUserDaoImpl.java
@@ -0,0 +1,73 @@
+package org.mnjaay.dao;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.mnjaay.database.HibernateConnection;
+import org.mnjaay.exceptions.DAOException;
+import org.mnjaay.model.User;
+
+import java.util.List;
+
+public class HibernateUserDaoImpl implements IDao<User> {
+
+    @Override
+    public void create(User user) throws DAOException {
+        try {
+            Session session = HibernateConnection.getInstance().getSession();
+            Transaction transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+        } catch (Exception e) {
+            throw new DAOException("ERROR : " + e.getClass() + " : " + e.getMessage());
+        }
+    }
+
+    @Override
+    public User read(int id) throws DAOException {
+        try {
+            Session session = HibernateConnection.getInstance().getSession();
+            return session.find(User.class, id);
+        } catch (Exception e) {
+            throw new DAOException("ERROR : " + e.getClass() + ":" + e.getMessage());
+        }
+    }
+
+    @Override
+    public List<User> list() throws DAOException {
+        try {
+            Session session = HibernateConnection.getInstance().getSession();
+            return session.createQuery("select u " + " from T_Users u", User.class).getResultList();
+        } catch (Exception e) {
+            throw new DAOException("ERROR : " + e.getClass() + ":" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void update(User entity) throws DAOException {
+        try {
+            Session session = HibernateConnection.getInstance().getSession();
+            Transaction transaction = session.beginTransaction();
+            session.merge(entity);
+            transaction.commit();
+
+        } catch (Exception e) {
+            throw new DAOException("ERROR : " + e.getClass() + ":" + e.getMessage());
+        }
+    }
+
+    @Override
+    public void delete(int id) throws DAOException {
+        try {
+            Session session = HibernateConnection.getInstance().getSession();
+            Transaction transaction = session.beginTransaction();
+            User user = read(id);
+            if (user != null) {
+                session.remove(user);
+            }
+
+            transaction.commit();
+        } catch (Exception e) {
+            throw new DAOException("ERROR : " + e.getClass() + ":" + e.getMessage());
+        }
+    }
+}
